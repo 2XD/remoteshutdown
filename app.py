46f1c433f1f060c06879bf936e9b7a57821a0c83
vm <81333771+2XD@users.noreply.github.com>
2025-02-26 04:14:24 -0600

    Update app.py
    fixed to show the correct ip to access to remote shutdown

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
+import socket
 import psutil
-import hashlib
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-import requests
 
 # Load environment and hashed secrets
 load_dotenv()
@@ -11,20 +10,21 @@ app = Flask(__name__)
 secret_key = os.getenv('SECRET_KEY')
 hashed_password = os.getenv('HASHED_PASSWORD')
 
-# shutdown the computer after 1 second using built in windows commands
+
+# Define the shutdown function
 def shutdown():
     if psutil.WINDOWS:
         os.system("shutdown /s /f /t 1")
     else:
         os.system("shutdown now")
 
-# Function to get public IP address
-def get_public_ip():
-    try:
-        ip = requests.get('https://api.ipify.org').text
-        return ip
-    except requests.RequestException:
-        return "Unable to fetch IP"
+
+# Get the local IP address dynamically
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
+
 
 # Route for shutdown
 @app.route('/shutdown', methods=['GET'])
@@ -32,23 +32,17 @@ def remote_shutdown():
     password = request.args.get('password')
     key = request.args.get('key')
     
-    # Get the client's IP address
-    client_ip = request.remote_addr
-    
     # Verify the password and key
     if password == hashed_password and key == secret_key:
         shutdown()
-        return jsonify({"message": f"Shutdown started from IP: {client_ip}!"}), 200
+        return jsonify({"message": "Shutdown started!"}), 200
     else:
         return jsonify({"ERROR": "Either the password or key is wrong."}), 403
 
+
 # Start the app
 if __name__ == '__main__':
-    # Get the public IP address dynamically
-    public_ip = get_public_ip()
-    
-    # Display the running server message with dynamic public IP
-    print(f"Running the server on http://{public_ip}:5000/")
-    print(f"To remote shutdown from your phone or another device, go to: http://{public_ip}:5000/shutdown?password={hashed_password}&key={secret_key}")
-    
+    local_ip = get_local_ip()  # Get local IP address
+    print(f"Running the server on http://{local_ip}:5000/")
+    print(f"To remote shutdown from your phone or another device, go to: http://{local_ip}:5000/shutdown?password={hashed_password}&key={secret_key}")
     app.run(host='0.0.0.0', port=5000)
